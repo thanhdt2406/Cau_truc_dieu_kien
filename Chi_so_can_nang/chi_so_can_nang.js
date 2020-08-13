16b24afe5456ecad0841eae9e2b0095a4b9aee9f
thanhdt246 <dothanh24062000@gmail.com>
2020-08-13 19:49:47 +0700

    edit exercise

--- Chi_so_can_nang/chi_so_can_nang.js
+++ Chi_so_can_nang/chi_so_can_nang.js
@@ -1,12 +1,16 @@
 function start() {
     let height = document.getElementById("height").value;
     let weight = document.getElementById("weight").value;
-    let bmi = weight / (height * height);
-    if (bmi < 18.5)
-        document.getElementById("result").innerHTML = "Result: Underweight";
-    else if (bmi < 25)
-             document.getElementById("result").innerHTML = "Result: Normal";
+    if (height <= 0 || weight <= 0) {
+        document.getElementById("result").innerHTML = "Error!";
+    } else {
+        let bmi = weight / (height * height);
+        if (bmi < 18.5)
+            document.getElementById("result").innerHTML = "Result: Underweight";
+        else if (bmi < 25)
+            document.getElementById("result").innerHTML = "Result: Normal";
         else if (bmi < 30)
-                document.getElementById("result").innerHTML = "Result: Overweight";
-             else document.getElementById("result").innerHTML = "Result: Obese";
+            document.getElementById("result").innerHTML = "Result: Overweight";
+        else document.getElementById("result").innerHTML = "Result: Obese";
+    }
 }
