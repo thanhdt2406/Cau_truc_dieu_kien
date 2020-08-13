72447069afdf57de6fd503c4918d3cff72372bc4
thanhdt246 <dothanh24062000@gmail.com>
2020-08-13 19:54:02 +0700

    edit exercise

--- Cau_lenh_switch-case/cau_lenh_switch-case.js
+++ Cau_lenh_switch-case/cau_lenh_switch-case.js
@@ -11,7 +11,17 @@ function dayOfMonth() {
         case 11:
             document.getElementById("result").innerHTML= "Tháng "+month+" có 30 ngày";
             break;
-        default:
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
             document.getElementById("result").innerHTML= "Tháng "+month+" có 31 ngày";
+            break;
+        default:
+            document.getElementById("result").innerHTML= "Error!";
+
     }
 }
